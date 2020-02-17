3f228c7871ab6864ce5abca7def4a4d910b98f3d
Gustavo Morais <gusxmorais@gmail.com>
2020-02-17 14:12:45 -0300

    Updates

---
+++ precache-manifest.7e6ebfd05bcfc56d80c610c629f095fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261d092f0e91eac668de288dbca8ef45",
+    "url": "/reactgram/index.html"
+  },
+  {
+    "revision": "62ed17fc490297a8f095",
+    "url": "/reactgram/static/js/2.f3d8e287.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/reactgram/static/js/2.f3d8e287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dfe9c4a68ed9940d35e",
+    "url": "/reactgram/static/js/main.f1c7083e.chunk.js"
+  },
+  {
+    "revision": "8baffb333b453b550e50",
+    "url": "/reactgram/static/js/runtime-main.431f7c40.js"
+  }
+]);
